6585fca1a0d004dd8b59548a140eec54e3f2aa58
Lukas Welzel <lwelzel@localhost.localdomain>
2021-03-02 22:51:43 +0100

    fixed issues due to rebasing, conflicting directories between cosmos2 and cosmos3

---
+++ ex4/welzel_ex4_q1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.renderers.default = 'browser'
+
+x=np.linspace(0.01,10,100)
+y=1/(np.exp(x)+np.exp(-x))
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=x, y=y, name="plot",
+                    line_shape='spline'))
+
+fig.update_xaxes(title_text='l/Ln')
+fig.update_yaxes(title_text='1/(e^(l/Ln)+e^(l/Ln)')
+
+fig.write_html("./ex4/welzel_ex4_1b_plot.html")
+fig.show()
--- ex4/welzel_ex4_q2.py
+++ ex4/welzel_ex4_q2.py
@@ -5,7 +5,7 @@ from astropy.visualization import astropy_mpl_style
 plt.style.use(astropy_mpl_style)
 from matplotlib import pyplot as plt
 
-img_raw=sh.read_img("./imgs_ex4/image1_2021.fits")
+img_raw=sh.read_img("./ex4/imgs_ex4/image1_2021.fits")
 img=np.array([row-np.mean(row) for row in img_raw])
 print(img_raw)
 print(img_raw-img)

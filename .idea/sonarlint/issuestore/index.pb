3525b4d15ab80f02228770f3ae08f7cd0dc3f8a3
Lukas Welzel <lwelzel@localhost.localdomain>
2021-03-26 10:32:37 +0100

    Update ex6

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,7 @@ D
 ex4/welzel_ex4_q2.py,b/a/ba19ef3b98efc43b15eb519d31af18cb1ceb2d5b
 D
 ex4/welzel_ex4_q4.py,c/7/c75b7dbc7920158b77925cf55875e9c128dc7091
+H
+ex4/welzel_ex4_master.py,6/b/6b8e8e5cc6d348cb82f8bf04ae22b681eb658e7f
+D
+ex6/welzel_ex6_qd.py,7/c/7c032442baed11018370a902aed066c82288731b
--- ex6/welzel_ex6_qd.py
+++ ex6/welzel_ex6_qd.py
@@ -37,6 +37,7 @@ class Circ:
         self.diss_P = None
 
         # e)
+        self.alpha = None
         self.NEP_J=None
 
         # f)
@@ -49,19 +50,19 @@ class Circ:
 
     def set_th_cond(self):
         self.th_cond = self.L * self.T * self.sigma
+        # self.th_cond = np.pi**2 * (1.380649e-23)**2 /(3* (1.602e-19)**2)* self.T * self.sigma
 
     def set_G(self):
         self.G = self.NEP_th ** 2 * self.q_eff ** 2 / (4 * 1.380649e-23 * self.T ** 2)
 
     def set_r_lead(self):
-        # th_cond is in cm convert to m
-        self.r_lead = np.sqrt(self.G * self.L / (self.th_cond*100 * np.pi * self.T))*100
+        self.r_lead = np.sqrt(self.G * self.l_lead / (self.th_cond * np.pi * 2))
 
     def set_diss_power(self):
         self.diss_P = self.V_out * self.I
 
     def find_load_curve_stupid_but_working(self):
-        for i in np.arange(1000):
+        for i in np.arange(100):
             self.V_out=(self.R0*self.I)/((1+(self.I*self.V_out/(self.G*self.T)))**4)
             self.I=(self.V_bias-self.V_out)/self.R_L
 
@@ -77,8 +78,13 @@ class Circ:
         self.V_out = res.x[0]
         self.I = (self.V_bias - self.V_out) /(self.R_L) # res.x[1]
 
+    def set_alpha(self):
+        self.alpha = -4/self.T
+
     def set_NEP_J(self):
-        self.NEP_J=self.G*self.T**(3/2)
+        # self.NEP_J = self.G*self.T**(3/2)
+        self.set_alpha()
+        self.NEP_J = np.sqrt(4*1.380649e-23*self.T/self.diss_P)*self.G/(self.q_eff*abs(self.alpha))
 
     def set_total_heat_cap_rat(self):
         self.C_v_lat = self.c_v_lat * self.px_a ** 3
@@ -88,12 +94,15 @@ class Circ:
 
 if __name__ == "__main__":
     BoloCirc = Circ(R0=1e6, R_L=1e7, V_bias=1.5, T0=2.7,
-                    c_v_lat=7e-6, c_v_e=1.3e-4, px_a=0.05, L=2.44e-8,T=2.7, sigma=3.1e5, NEP_th=4.5e-15,
+                    c_v_lat=7e-6, c_v_e=1.3e-4, px_a=0.05, L=2.44e-8, T=2.7, sigma=3.1e5, NEP_th=4.5e-15,
                     q_eff=0.5,l_lead=1)
-    if True:
-        BoloCirc = Circ(R0=1e6, R_L=1e7, V_bias=1.5, T0=2.7,
-                        c_v_lat=7e-6, c_v_e=1.3e-4, px_a=0.05, L=2.44e-8, T=2.7, sigma=3.1e5, NEP_th=4.5e-15,
-                        q_eff=0.5, l_lead=1, G=0.22e-6)
+
+    # check code with exercise example
+    unit_test = False
+    if unit_test:
+        BoloCirc = Circ(R0=1e6, R_L=1e7, V_bias=1.5, T0=1.5,
+                        c_v_lat=7e-6, c_v_e=1.3e-4, px_a=0.053, L=2.44e-8, T=1.5, sigma=3.1e5, NEP_th=5.8e-15,
+                        q_eff=0.9, l_lead=0.5)
 
     # b)
     print("\n--=== Q b) ===---")
@@ -104,8 +113,8 @@ if __name__ == "__main__":
 
     try:
         print(f"th_cond: {BoloCirc.th_cond:.3E} W K cm-1\n"
-              f"G:       {BoloCirc.G:.3E} S m-1\n"
-              f"r_lead:  {BoloCirc.r_lead:.3E} cm")
+              f"G:       {BoloCirc.G:.3E} W K-1\n"
+              f"r_lead:  {BoloCirc.r_lead:.3E} cm (D = {2* BoloCirc.r_lead/100*1e6:.3E} micron)")
     except TypeError:
         print(f"G:       {BoloCirc.G:.3E} S m-1\n")
 
@@ -118,10 +127,6 @@ if __name__ == "__main__":
               f"I:       {BoloCirc.I:.3E} A\n"
               f"diss. P: {BoloCirc.diss_P:.3E} W")
 
-        # V_axis=np.linspace(0,2,100)
-        # I_axis=np.linspace(0,1e-6,100)
-        # grid_xx, grid_yy = np.meshgrid(V_axis, I_axis, indexing='xy')
-
         # e)
         print("\n--=== Q e) ===---")
         BoloCirc.set_NEP_J()
@@ -130,6 +135,34 @@ if __name__ == "__main__":
         # f)
         print("\n--=== Q f) ===---")
         BoloCirc.set_total_heat_cap_rat()
-        print(f"C_v_total_ratio: {BoloCirc.C_v_total:.3E} [-]")
+        print(f"C_v_total_ratio at 2.7 K: {BoloCirc.C_v_total*(2.7)**(3-1):.3E} [-]")
+        print(f"C_v_total_ratio is unity at: {np.sqrt(1/BoloCirc.C_v_total):.3E} K")
+
+        T_range=np.linspace(1e-1,4,10000)
+        C_v_total_ratio=BoloCirc.C_v_total*(T_range)**(3-1)
+
+
+        fig, ax1, = plt.subplots(1, 1, constrained_layout=False)
+
+        point = ax1.scatter(x=T_range, y=C_v_total_ratio, s=1, label=r"general curve")
+        unity = ax1.scatter(x=2.7, y=BoloCirc.C_v_total*(2.7)**(3-1), s=80, marker='+',c="red", label=r"ratio @ 2.7 K" )
+        ax1.scatter(x=np.sqrt(1/BoloCirc.C_v_total), y=1, s=80, marker='+', c="green", label=r"ratio is unity")
+
+        plt.rc('text', usetex=True)
+        ax1.set_title(f'Ratio of total heat capacities vs Temperature', fontsize=12)
+        ax1.set_ylabel(r'Ratio of total heat capacities [$C_{v}^{{lat }} / C_{v}^{e}$]')
+        ax1.set_xlabel('Temperature [$T$]')
+
+        ax1.loglog()
+        ax1.grid(which="both", alpha=0.5, )
+        ax1.set_axisbelow(True)
+        # plt.legend() idk why it doesnt want to print tex in legend
+
+        plt.tight_layout()
+        fig.set_dpi(500)
+        plt.savefig("./welzel_ex6_qf_plot.png", dpi=500)
+        plt.show()
+        plt.clf()
+
     except TypeError:
         print("\nRunning test code. No further outputs.")
